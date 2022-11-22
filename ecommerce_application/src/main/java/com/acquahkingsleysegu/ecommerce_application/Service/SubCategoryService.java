6a2f3072f206a652020c8204624598e2b801402c
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-11-22 18:06:19 +0100

    Service interface for sub category object

--- ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Service/SubCategoryService.java
+++ ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Service/SubCategoryService.java
@@ -1,2 +1,17 @@
-package com.acquahkingsleysegu.ecommerce_application.Service;public interface SubCategoryService {
+package com.acquahkingsleysegu.ecommerce_application.Service;
+
+import com.acquahkingsleysegu.ecommerce_application.Entity.SubCategoryEntity;
+import com.acquahkingsleysegu.ecommerce_application.Model.SubCategoryModel;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface SubCategoryService {
+    SubCategoryModel createSubCategory(SubCategoryModel subCategoryModel);
+    List<SubCategoryModel> getSubCategories();
+
+    SubCategoryModel getSubCategoryById(Long id);
+    SubCategoryModel getSubCategoryByName(String name);
+    SubCategoryModel updateSubCategory(Long id, SubCategoryModel subCategoryModel);
+    boolean deleteSubCategory(Long id);
 }
