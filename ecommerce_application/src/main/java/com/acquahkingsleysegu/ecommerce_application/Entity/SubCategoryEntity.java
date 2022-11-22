3f93773786c02c210f42ed791563f41b4517f4e6
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-11-22 17:59:08 +0100

    Spring JPA entity class for sub category object

--- ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Entity/SubCategoryEntity.java
+++ ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Entity/SubCategoryEntity.java
@@ -1,22 +1,26 @@
 package com.acquahkingsleysegu.ecommerce_application.Entity;
 
 import lombok.Data;
+import lombok.Getter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "sub_category")
 @Data
-public class SubCategory {
+public class SubCategoryEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "scat_id")
     private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "main_id")
-    private MainCategoryEntity mainCategory;
-
     private String name;
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "mainID", referencedColumnName = "id")
+    private MainCategoryEntity mainCategoryID;
+
+    @OneToMany(targetEntity = ItemEntity.class, mappedBy = "subCategoryEntity", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    List<ItemEntity> itemEntities;
 }
