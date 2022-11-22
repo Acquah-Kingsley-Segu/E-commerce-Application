4c12de3231ab48e62982df0d25dd7aeff68df1b7
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-11-22 18:02:45 +0100

    Spring JPA repository to communicate with main category database table

--- ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Repository/MainCategoryRepository.java
+++ ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Repository/MainCategoryRepository.java
@@ -1,2 +1,11 @@
-package com.acquahkingsleysegu.ecommerce_application.Repository;public interface MainCategoryRepository {
+package com.acquahkingsleysegu.ecommerce_application.Repository;
+
+import com.acquahkingsleysegu.ecommerce_application.Entity.MainCategoryEntity;
+import com.acquahkingsleysegu.ecommerce_application.Entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MainCategoryRepository extends JpaRepository<MainCategoryEntity, Long> {
+    MainCategoryEntity findByName(String name);
 }
