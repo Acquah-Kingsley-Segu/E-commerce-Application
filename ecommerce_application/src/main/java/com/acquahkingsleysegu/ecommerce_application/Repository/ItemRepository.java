36e2788514484e2f8062aeb082978ceef9a990ce
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-11-22 18:02:31 +0100

    Spring JPA repository to communicate with item database table

--- ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Repository/ItemRepository.java
+++ ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Repository/ItemRepository.java
@@ -1,2 +1,10 @@
-package com.acquahkingsleysegu.ecommerce_application.Repository;public interface ItemRepository {
+package com.acquahkingsleysegu.ecommerce_application.Repository;
+
+import com.acquahkingsleysegu.ecommerce_application.Entity.ItemEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
+    ItemEntity findByName(String name);
 }
