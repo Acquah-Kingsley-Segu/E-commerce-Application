efa6843ab431e581da3d5d004f28104ef5a205b3
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-11-22 17:58:38 +0100

    Spring JPA entity class for item object

--- ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Entity/ItemEntity.java
+++ ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Entity/ItemEntity.java
@@ -1,2 +1,21 @@
-package com.acquahkingsleysegu.ecommerce_application.Entity;public class ItemEntity {
+package com.acquahkingsleysegu.ecommerce_application.Entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item")
+@Data
+public class ItemEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer qty;
+    @ManyToOne
+    @JoinColumn(name = "cat_id", referencedColumnName = "scat_id")
+    private SubCategoryEntity subCategoryEntity;
 }
