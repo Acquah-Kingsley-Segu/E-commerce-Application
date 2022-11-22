c209d13a0ecf4fd79f8419e6eb95d06edcc2067a
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-11-22 18:01:00 +0100

    MVC model for sub category entity class

--- ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Model/SubCategoryModel.java
+++ ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Model/SubCategoryModel.java
@@ -1,2 +1,20 @@
-package com.acquahkingsleysegu.ecommerce_application.Model;public class SubCategoryModel {
+package com.acquahkingsleysegu.ecommerce_application.Model;
+
+import com.acquahkingsleysegu.ecommerce_application.Entity.ItemEntity;
+import com.acquahkingsleysegu.ecommerce_application.Entity.MainCategoryEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubCategoryModel {
+    private Long Id;
+    private String name;
+    private String description;
+    private MainCategoryEntity mainCategoryID;
+    List<ItemEntity> itemEntities;
 }
