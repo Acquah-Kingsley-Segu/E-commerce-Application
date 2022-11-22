4a84e84639ff4f5e35db1adf8135939d17e59bc2
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-11-22 18:03:00 +0100

    Spring JPA repository to communicate with sub category database table

--- ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Repository/SubCategoryRepository.java
+++ ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Repository/SubCategoryRepository.java
@@ -1,2 +1,10 @@
-package com.acquahkingsleysegu.ecommerce_application.Repository;public interface SubCategoryRepository {
+package com.acquahkingsleysegu.ecommerce_application.Repository;
+
+import com.acquahkingsleysegu.ecommerce_application.Entity.SubCategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubCategoryRepository extends JpaRepository<SubCategoryEntity, Long> {
+    SubCategoryEntity findByName(String name);
 }
