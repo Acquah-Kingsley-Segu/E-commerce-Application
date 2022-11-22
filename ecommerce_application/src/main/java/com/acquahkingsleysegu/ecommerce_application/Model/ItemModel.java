3ee9d7f99cdcb11b196c1fa0002aa2507a41f12a
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-11-22 18:00:22 +0100

    MVC model for item entity class

--- ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Model/ItemModel.java
+++ ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Model/ItemModel.java
@@ -1,2 +1,18 @@
-package com.acquahkingsleysegu.ecommerce_application.Model;public class ItemModel {
+package com.acquahkingsleysegu.ecommerce_application.Model;
+
+import com.acquahkingsleysegu.ecommerce_application.Entity.SubCategoryEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ItemModel {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer qty;
+    private SubCategoryEntity subCategoryEntity;
 }
