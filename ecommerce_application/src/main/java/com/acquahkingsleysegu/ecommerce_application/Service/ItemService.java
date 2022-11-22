cdd57bfd73f0be2ad76727ac1c3033fd35839014
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-11-22 18:05:13 +0100

    Service interface for item object

--- ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Service/ItemService.java
+++ ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Service/ItemService.java
@@ -1,2 +1,15 @@
-package com.acquahkingsleysegu.ecommerce_application.Service;public interface ItemService {
+package com.acquahkingsleysegu.ecommerce_application.Service;
+
+import com.acquahkingsleysegu.ecommerce_application.Model.ItemModel;
+import com.acquahkingsleysegu.ecommerce_application.Model.SubCategoryModel;
+
+import java.util.List;
+
+public interface ItemService {
+    ItemModel createItem(ItemModel item);
+    List<ItemModel> getItems();
+    ItemModel getItemById(Long id);
+    ItemModel getItemByName(String name);
+    ItemModel updateItem(Long id, ItemModel subCategoryModel);
+    boolean deleteItem(Long id);
 }
