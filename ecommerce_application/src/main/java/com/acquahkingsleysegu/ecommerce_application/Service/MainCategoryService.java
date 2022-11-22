e89bdc0df7b5f3bb3b7a42556fc304fe72531cd9
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-11-22 18:06:07 +0100

    Service interface for main category object

--- ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Service/MainCategoryService.java
+++ ecommerce_application/src/main/java/com/acquahkingsleysegu/ecommerce_application/Service/MainCategoryService.java
@@ -1,2 +1,17 @@
-package com.acquahkingsleysegu.ecommerce_application.Service;public interface MainCategoryService {
+package com.acquahkingsleysegu.ecommerce_application.Service;
+
+import com.acquahkingsleysegu.ecommerce_application.Entity.MainCategoryEntity;
+import com.acquahkingsleysegu.ecommerce_application.Entity.SubCategoryEntity;
+import com.acquahkingsleysegu.ecommerce_application.Model.MainCategoryModel;
+
+import java.util.List;
+
+
+public interface MainCategoryService {
+    MainCategoryModel createCategory(MainCategoryModel category);
+    List<MainCategoryModel> getCategories();
+    MainCategoryModel getCategoryById(Long id);
+    MainCategoryModel getCategoryByName(String name);
+    MainCategoryModel updateCategory(Long id, MainCategoryModel category);
+    boolean deleteCategory(Long id);
 }
